dac97482b5911831dd3285df03519416b44254d3
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2015-01-26 03:35:48 +0700

    display all active keys

--- src/elements/note-manager/script.js
+++ src/elements/note-manager/script.js
@@ -3,6 +3,7 @@ Polymer({
   created() {
     this._active = { }
     this.params = { }
+    this.activeNotes = { }
   },
   ready() {
   },
@@ -11,12 +12,21 @@ Polymer({
     let gain = velocity / 127
     let freq = Math.pow(2, (noteTransposed - 69) / 12) * 440
     this._queueFor(note).push(this.synthesizer.note({ freq, vel: gain }))
+    this._update()
   },
   noteOff(note, velocity) {
     this._queueFor(note).shift().release()
+    this._update()
   },
   _queueFor(note) {
     return this._active[note] || (this._active[note] = [])
+  },
+  _update() {
+    let active = { }
+    for (let k in this._active) {
+      if (this._active[k] && this._active[k].length) active[k] = true
+    }
+    this.activeNotes = active
   },
 })
 
